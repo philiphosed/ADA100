dff417a30bff0f831cbb07b4db5c43689e4b2a63
David Philiphose <dmpme@hotmail.copm>
2022-04-25 18:36:32 -0500

    added first lab

---
+++ Night_1/lab01.adb
@@ -0,0 +1,11 @@
+with Ada.Text_IO;
+
+procedure lab01 is
+begin
+   Ada.Text_IO.Put("David M. Philiphose");
+   Ada.Text_IO.New_Line;
+   Ada.Text_IO.Put("123 Arsenal Street");
+   Ada.Text_IO.New_Line;
+   Ada.Text_IO.Put("St. Louis, MO 63118");
+   Ada.Text_IO.New_Line;
+end;
