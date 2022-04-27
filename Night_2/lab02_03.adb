ade96017c8ca921070a6dbe29d30865128bd7c77
David Philiphose <dmpme@hotmail.copm>
2022-04-27 18:16:12 -0500

    added start of night 2 notes and added in lab 2 and lab 3 combined

---
+++ Night_2/lab02_03.adb
@@ -0,0 +1,32 @@
+with Ada.Text_IO, Ada.Integer_IO;
+use Ada.Text_IO, Ada.Integer_IO;
+
+procedure lab02_03 is
+   int1, int2, int3 : Integer;
+
+   procedure doSum (int1 : in Integer; int2 : in Integer; int3 : out Integer) is
+
+   begin
+
+      int3 := int1 + int2;
+
+   end;
+
+
+begin
+
+   Ada.Text_IO.Put_Line("Enter First Int:");
+
+   Ada.Integer_IO.Get(int1);
+
+   Ada.Text_IO.Put_Line("Enter Second Int:");
+
+   Ada.Integer_IO.Get(int2);
+
+   doSum(int1,int2,int3);
+
+   Ada.Text_IO.Put("Sum of First and Second Ints");
+
+   Ada.Integer_IO.Put(int3);
+
+end;
